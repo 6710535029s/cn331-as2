b1aefe44c2d5f892fd0818429e83996c470a552f
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 04:26:42 +0700

    Update models.py

--- booking_room/booking/models.py
+++ booking_room/booking/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
-
-# class room model
 class Classroom(models.Model):
-    room_code = models.CharField(max_length= 64, unique=True) # กำหนดให้room_codeไม่ซ้ำกัน
-    room_name = models.CharField(max_length= 128) # ชื่อห้อง
-    room_capacity = models.PositiveIntegerField() # ความจุของห้อง
-    available_hours = models.PositiveIntegerField() # ชั่วโมงที่ว่างของห้องนั้น
-    status = models.BooleanField(default=True) #ส ถานะขอห้อง ณ ขณะนั้น
-    
-# booking model
-class Booking(models.Model): 
-    user = models.ForeignKey(User, on_delete=models.CASCADE) #เชื่อมโยงกับ user model 
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE) #เชื่อมโยงกับ room model
-    booking_date = models.DateField(auto_now_add=True) # วันที่จอง 
-    start_time = models.TimeField() # เวลาที่เริ่มจอง
-    end_time = models.TimeField() # เวลาที่สิ้นสุดการจอง
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    total_hours = models.IntegerField()
+    is_available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    date = models.DateField()
+    hour = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.classroom.name} - {self.date} - {self.hour}"
