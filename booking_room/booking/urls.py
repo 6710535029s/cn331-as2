ce7c9dc9f95a462a06afe78005339342763a7ff0
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 18:56:33 +0700

    Update urls.py

--- booking_room/booking/urls.py
+++ booking_room/booking/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', views.home_view, name='home_view'),
+    path('home/', views.home_view, name='home_view'),
     path('userprofile/', views.user_profile_view, name='user_profile'),
+    path('profile/', views.user_profile_view, name='user_profile'),  # สำรอง
     path('book/<int:classroom_id>/', views.booking_page, name='booking_page'),
-    path('home/', views.home_view, name='home'),
+    path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
 ]
