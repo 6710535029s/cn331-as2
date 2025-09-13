af2d9b2dce42f3e623e332243b9861bdfedb132b
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 13:43:45 +0700

    f

--- booking_room/booking/admin.py
+++ booking_room/booking/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Classroom
-
-@admin.register(Classroom) #ใช้ decorator @admin.register เพื่อบอก Django ว่าเราจะจัดการโมเดล Classroom ด้วยคลาส ClassroomAdmin
+from .models import Classroom, Booking 
+#ตั้งค่าการแสดงผลของโมเดลห้องเรียน
+@admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
-  # กำหนดว่าในหน้า รายการห้องเรียน (list view) จะแสดงคอลัมน์อะไรบ้าง เช่น: รหัสห้อง, ชื่อห้อง, ความจุ, จำนวนชั่วโมงที่เปิดให้จอง, สถานะเปิด/ปิด
-    list_display = ('code', 'name', 'capacity', 'total_hours', 'is_available')
-  # เพิ่ม ตัวกรองด้านข้าง ให้ admin สามารถกรองห้องเรียนที่เปิดหรือปิดการจองได้ง่าย ๆ
-    list_filter = ('is_available',)
-  # เพิ่มช่องค้นหาให้ admin สามารถค้นหาห้องเรียนตาม code หรือ name ได้สะดวก
-    search_fields = ('code', 'name')
+    list_display = ['code', 'name', 'capacity', 'status']
+    search_fields = ['code', 'name']
+    list_filter = ['status']
+#ตั้งค่าการแสดงผลของโมเดลการจอง
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['classroom', 'user', 'date', 'hour']
+    list_filter = ['date', 'classroom']
+    search_fields = ['user__username']
