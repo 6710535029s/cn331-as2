6ecab58e5e1d0f9b3386617d2a26ccaf8580379a
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 14:10:19 +0700

    Update urls.py

--- booking_room/booking/urls.py
+++ booking_room/booking/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
     path('userprofile/', views.user_profile_view, name='user_profile'),
+    path('book/<int:classroom_id>/', views.booking_page, name='booking_page'),
 ]
+
