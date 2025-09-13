63ac455bba9b2a41f1966dd171a8ad2244fc5ef8
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 15:36:55 +0700

    Update views.py

--- booking_room/accounts/views.py
+++ booking_room/accounts/views.py
@@ -21,22 +21,22 @@ def register_view(request):
 
     return render(request, 'accounts/register.html')
 
-
 # ฟังก์ชันเข้าสู่ระบบ
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-
         user = authenticate(request, username=username, password=password)
-        if user:
+        if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.error(request, 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง')
-
-    return render(request, 'accounts/login.html')
-
+    return render(request, 'booking/login.html') 
 
 # ฟังก์ชันออกจากระบบ
 def logout_view(request):
