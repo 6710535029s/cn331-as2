173bb65880acb46d2690cb4c4686382a6a4264bd
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 15:04:55 +0700

    Update views.py

--- booking_room/booking/views.py
+++ booking_room/booking/views.py
@@ -1,5 +1,5 @@
+from django.db import IntegrityError
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -8,26 +8,27 @@ from .models import Classroom, Booking
 from datetime import time
 
 # หน้าแรกของระบบ (หลัง login)
+@login_required(login_url='login')
 def home_view(request):
     classrooms = Classroom.objects.filter(status='available')
     return render(request, 'booking/home.html', {'classrooms': classrooms})
-    
+
 # หน้าโปรไฟล์ผู้ใช้
 @login_required(login_url='login')
 def user_profile_view(request):
     bookings = Booking.objects.filter(user=request.user)
     return render(request, 'booking/user_profile.html', {'bookings': bookings})
 
-# หน้าจองห้องเรียน
-@login_required(login_url='login')
-def booking_page(request, classroom_id):
-    classroom = get_object_or_404(Classroom, id=classroom_id)
+@login_required
+def cancel_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('user_profile')
     
-return render(request, 'booking/booking_page.html', context)
+#mybooking
+@login_required
+def booking_page(request, classroom_id):
 
 
-
-
-
-
-
+    return render(request, 'booking/my_bookings.html', context)
