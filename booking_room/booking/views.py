4ab5cf0c323003687bd6b8cc6925fdeea17652e7
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 13:33:01 +0700

    Update views.py

--- booking_room/booking/views.py
+++ booking_room/booking/views.py
@@ -44,11 +44,12 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
-
+# หน้าโปรไฟล์ผู้ใช้
 @login_required(login_url='login')
 def user_profile_view(request):
     bookings = Booking.objects.filter(user=request.user)
     return render(request, 'booking/user_profile.html', {'bookings': bookings})
+
 
 
 
