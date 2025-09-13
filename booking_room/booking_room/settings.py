332abf239059bd817b7a7852d4fdb5dfced63cf8
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 13:47:15 +0700

    replaced app booking to booking.apps.BookingConfig

--- booking_room/booking_room/settings.py
+++ booking_room/booking_room/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking', # add app booking
+    'booking.apps.BookingConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
