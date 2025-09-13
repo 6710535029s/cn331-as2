a897d127447a0c9c061d8d9f4e27666e823eb083
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 13:11:45 +0700

    add userprofile

--- booking_room/booking/urls.py
+++ booking_room/booking/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
-    path('book/', views.book_room_view, name='book_room'),
-    path('my-bookings/', views.my_bookings_view, name='my_bookings'),
+    path('userprofile/', views.user_profile_view, name='user_profile'),
 ]
