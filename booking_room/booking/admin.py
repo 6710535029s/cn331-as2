5b8c0dfc170b62f2be791a49e6b8c37d64998d07
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 13:44:50 +0700

    fix some error

--- booking_room/booking/admin.py
+++ booking_room/booking/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Classroom, Booking 
-#ตั้งค่าการแสดงผลของโมเดลห้องเรียน
+
 @admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'capacity', 'status']
     search_fields = ['code', 'name']
     list_filter = ['status']
-#ตั้งค่าการแสดงผลของโมเดลการจอง
+
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ['classroom', 'user', 'date', 'hour']
