046732974b182b7ea52193aad0573506d3a7ebfd
6710535029 <6710535029@student.tu.ac.th>
2025-09-13 15:12:30 +0700

    Update settings.py

--- booking_room/booking_room/settings.py
+++ booking_room/booking_room/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking.apps.BookingConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
 
